86fae593546f338974187e6cb058e03b6d67f34d
Nicolas Trangez <ikke@nicolast.be>
2014-06-24 18:23:10 +0200

    Remove unsafe stuff

--- driver.hs
+++ driver.hs
@@ -1,23 +1,25 @@
+module Main (main) where
+
+import Control.Monad
 import qualified System.Directory as D
-import qualified System.IO.Unsafe as SIU
 
 outputDir = "data"
 rawExt = ".xml"
 
-
-mapMI :: (a -> IO b) -> [a] -> IO [b]
-mapMI _ [] 	= return []
-mapMI f (x:xs) 	= do y <- SIU.unsafeInterleaveIO (f x) ; ys <- SIU.unsafeInterleaveIO (mapMI f xs) ; return (y:ys)
-
+processFileContent :: [a] -> Int
 processFileContent = length
 
+getBugs :: IO [Int]
 getBugs = do
         files <- D.getDirectoryContents outputDir
         let xmlFiles = map (\n -> outputDir ++ "/" ++ n) $
                        filter (\n -> (reverse . take 4 . reverse) n == rawExt) files
-        mapMI (\f -> do {str <- readFile f; return (processFileContent str); }) (xmlFiles)
+        forM xmlFiles $ \n -> do
+            str <- readFile n
+            let l = processFileContent str
+            l `seq` return l
 
-
+main :: IO ()
 main = do
 	x <- getBugs
 	putStrLn (show x)
